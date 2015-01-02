c6ea27c0045f5d5fefbcc24739bb91bcbc3d76c0
Vadim <mail@vadimklimenko.com>
2015-01-02 20:34:34 +0200

    Improve code

--- manage.py
+++ manage.py
@@ -1,41 +1,35 @@
-from pyfirmata import ArduinoMega, util
-board = ArduinoMega('/dev/tty.usbmodem1421')
-
-pinR = board.get_pin('d:4:p')
-pinG = board.get_pin('d:3:p')
-pinB = board.get_pin('d:2:p')
+import time
+import threading
 
 import os
-import time
 from PIL import Image
+from pyfirmata import ArduinoMega, util
 
-import time, threading
 
+board = ArduinoMega('/dev/tty.usbmodem1421')
 
-def foo(r=0, g=0, b=0):
-	os.system("screencapture -x screen.jpg")
+CENTER_LED = [{'r': board.get_pin('d:4:p'), 'g': board.get_pin('d:3:p'), 'b': board.get_pin('d:2:p')}, [0, 0, 0],
+              [0, 0, 0]]
 
-	old_r = r
-	old_g = g
-	old_b = b
+while True:
+    os.system("screencapture -x screen.jpg")
+    CENTER_LED[1] = CENTER_LED[2][:]
 
-	im = Image.open('screen.jpg')
-	rgb_im = im.convert('RGB')
-	r, g, b = rgb_im.getpixel((im.size[0]/2, im.size[1]/2))
-	r = float(r)/255
-	g = float(g)/255
-	b = float(b)/255
+    im = Image.open('screen.jpg')
+    rgb_im = im.convert('RGB')
+    temp_r, temp_g, temp_b = rgb_im.getpixel((im.size[0] / 2, im.size[1] / 2))
+    CENTER_LED[2][0] = float(temp_r) / 255
+    CENTER_LED[2][1] = float(temp_g) / 255
+    CENTER_LED[2][2] = float(temp_b) / 255
 
-	step_r = (old_r-r)/100
-	step_g = (old_g-g)/100
-	step_b = (old_b-b)/100
+    step_r = (CENTER_LED[1][0] - CENTER_LED[2][0]) / 100
+    step_g = (CENTER_LED[1][1] - CENTER_LED[2][1]) / 100
+    step_b = (CENTER_LED[1][2] - CENTER_LED[2][2]) / 100
 
-	for n in range(1, 100):
-		pinR.write(old_r-step_r*n)
-		pinG.write(old_g-step_g*n)
-		pinB.write(old_b-step_b*n)
-		time.sleep(0.005)
+    # print(str(CENTER_LED[1][0]) + " " + str(CENTER_LED[2][0]))
 
-	# time.sleep(1)
-	foo(r=r, g=g, b=b)
-foo(0, 0, 0)
+    for n in range(1, 100):
+        CENTER_LED[0]['r'].write(CENTER_LED[1][0] - step_r * n)
+        CENTER_LED[0]['g'].write(CENTER_LED[1][1] - step_g * n)
+        CENTER_LED[0]['b'].write(CENTER_LED[1][2] - step_b * n)
+        time.sleep(0.005)
