68d5b006fb23180fbc64b93eb722ef6f0105b0d1
Vadim <mail@vadimklimenko.com>
2015-01-03 13:24:28 +0200

    Refactoring

--- manage.py
+++ manage.py
@@ -8,28 +8,62 @@ from pyfirmata import ArduinoMega, util
 
 board = ArduinoMega('/dev/tty.usbmodem1421')
 
-CENTER_LED = [{'r': board.get_pin('d:4:p'), 'g': board.get_pin('d:3:p'), 'b': board.get_pin('d:2:p')}, [0, 0, 0],
-              [0, 0, 0]]
+# CENTER_LED = [{'r': board.get_pin('d:4:p'), 'g': board.get_pin('d:3:p'), 'b': board.get_pin('d:2:p')}, [0, 0, 0],
+# [0, 0, 0]]
+# LEFTLED = [{'r': board.get_pin('d:5:p'), 'g': board.get_pin('d:7:p'), 'b': board.get_pin('d:6:p')}, [0, 0, 0],
+#            [0, 0, 0]]
+# RIGHTLED = [{'r': board.get_pin('d:10:p'), 'g': board.get_pin('d:9:p'), 'b': board.get_pin('d:8:p')}, [0, 0, 0],
+#             [0, 0, 0]]
+
+
+class LED:
+    pins = None
+    current_colors = [0, 0, 0]
+    old_colors = [0, 0, 0]
+    position = 1  # 1 - left, 2 - center, 3 - right
+    board = None
+
+    def __init__(self, board, pins, position):
+        self.position = position
+        self.board = board
+        self.pins = [self.board.get_pin('d:' + str(pin) + ':p') for pin in pins]
+
+    def update_colors(self, picture):
+        self.old_colors = self.current_colors[:]
+
+        position_coords = {
+            1: picture.getpixel((0, im.size[1] / 2)),
+            2: picture.getpixel((im.size[0] / 2, im.size[1] / 2)),
+            3: picture.getpixel((im.size[0]-1, im.size[1] / 2))
+        }
+
+        temp_colors = position_coords.get(self.position)
+
+        self.current_colors = [float(color) / 255 for color in temp_colors]
+
+        step_r = (self.old_colors[0] - self.current_colors[0]) / 100
+        step_g = (self.old_colors[1] - self.current_colors[1]) / 100
+        step_b = (self.old_colors[2] - self.current_colors[2]) / 100
+
+        for n in range(1, 100):
+            self.draw(0, self.old_colors[0] - step_r * n)
+            self.draw(1, self.old_colors[1] - step_g * n)
+            self.draw(2, self.old_colors[2] - step_b * n)
+            time.sleep(0.005)
+
+    def draw(self, pin, value):
+        self.pins[pin].write(value)
+
+led_left = LED(board, [4, 3, 2], 1)
+led_center = LED(board, [5, 7, 6], 2)
+led_right = LED(board, [10, 9, 8], 3)
 
 while True:
     os.system("screencapture -x screen.jpg")
-    CENTER_LED[1] = CENTER_LED[2][:]
 
     im = Image.open('screen.jpg')
     rgb_im = im.convert('RGB')
-    temp_r, temp_g, temp_b = rgb_im.getpixel((im.size[0] / 2, im.size[1] / 2))
-    CENTER_LED[2][0] = float(temp_r) / 255
-    CENTER_LED[2][1] = float(temp_g) / 255
-    CENTER_LED[2][2] = float(temp_b) / 255
 
-    step_r = (CENTER_LED[1][0] - CENTER_LED[2][0]) / 100
-    step_g = (CENTER_LED[1][1] - CENTER_LED[2][1]) / 100
-    step_b = (CENTER_LED[1][2] - CENTER_LED[2][2]) / 100
-
-    # print(str(CENTER_LED[1][0]) + " " + str(CENTER_LED[2][0]))
-
-    for n in range(1, 100):
-        CENTER_LED[0]['r'].write(CENTER_LED[1][0] - step_r * n)
-        CENTER_LED[0]['g'].write(CENTER_LED[1][1] - step_g * n)
-        CENTER_LED[0]['b'].write(CENTER_LED[1][2] - step_b * n)
-        time.sleep(0.005)
+    led_left.update_colors(rgb_im)
+    led_center.update_colors(rgb_im)
+    led_right.update_colors(rgb_im)
